14a487c67af0827d2acd28711ee85cdda81a8c5b
chrisfaux95 <chrisfaux95@gmail.com>
2020-08-11 14:31:07 -0400

    added questions and basic prompt

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 var inquirer = require('inquirer')
 
+var questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: "Project Title?"
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: "What is your Project?"
+    },
+    {
+        type: 'list',
+        name: 'language',
+        message: "What are you coding this in?",
+        choices: ['JS', 'Python', 'Full Web', 'Java', 'Ruby']
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license would you like to use?',
+        choices: [
+            'MIT',
+            'GNU'
+        ]
+    }
+]
+
+inquirer.prompt(questions).then((answers) => console.log(answers))
