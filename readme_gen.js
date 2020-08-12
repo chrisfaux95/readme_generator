d49be2843663abd0088dd094984018c28c4e9f0b
chrisfaux95 <chrisfaux95@gmail.com>
2020-08-12 10:37:06 -0400

    expanded createReadmeText

--- readme_gen.js
+++ readme_gen.js
@@ -1,17 +1,45 @@
 
 
 
-function createReadmeText(input){
-    return `# ${input.title}
-    ${input.description}
+function createReadmeText(input) {
+    return `
+![Custom badge](https://img.shields.io/endpoint)  
+# ${input.title}
 
-    ---
-    ### Table Of Contents
-    
-    
-    
-    
-    
+${input.description}
+---
+### Table Of Contents
+ 
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [License](#license)
+* [Contact](#contact)
+
+---
+
+## Installation
+ 
+${input.install} 
+
+## Usage
+
+${input.usage}
+
+## Contributing
+
+${input.contribute}
+
+## Contact
+
+${contactInfo(input)}
+
+## License
+
+${licenseInfo(input)}
+`
+}
+
     
     `
 }
